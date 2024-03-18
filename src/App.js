ff6da66541d53fb02ef5f27c1982c1ceb346f855
Your Name <you@example.com>
2024-03-18 23:19:41 +0530

    added Profile And notes component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import RegisterPage from "./components/Register/Register"
 import RegisterPage from "./pages/RegisterPage/RegisterPage"
 import MoviePage from "./pages/MoviePage/MoviePage"
+import HomePage from "./pages/HomePage/HomePage"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={ <HomePage /> } />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/genre" element={<MoviePage />} />
       </Routes>
